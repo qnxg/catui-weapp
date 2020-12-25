e26f1570f75f06c8c4b3d310182d36e69fcf3048
wangjq4214 <w1637894214@163.com>
2020-12-25 16:52:01 +0800

    feat: 补充 loading 实例

--- packages/catui-weapp-demo/src/components/showArea/index.tsx
+++ packages/catui-weapp-demo/src/components/showArea/index.tsx
@@ -1,15 +1,18 @@
 import React, { FC } from 'react';
 import { View } from '@tarojs/components';
 
+import './index.scss';
+
 interface Props {
   title: string;
+  padding?: boolean;
 }
 
-const Index: FC<Props> = ({ title, children }) => {
+const Index: FC<Props> = ({ title, padding, children }) => {
   return (
-    <View>
-      <View>{title}</View>
-      {children}
+    <View className={`demo-block ${padding ? 'demo-block--padding' : ''}`}>
+      <View className='demo-block__title'>{title}</View>
+      <View className='demo-block__card'>{children}</View>
     </View>
   );
 };
--- packages/catui-weapp-demo/src/pages/loading/index.tsx
+++ packages/catui-weapp-demo/src/pages/loading/index.tsx
@@ -2,30 +2,32 @@ import React from 'react';
 import { View } from '@tarojs/components';
 import { CatLoading } from 'catui-weapp';
 
+import ShowArea from '../../components/showArea';
+
 import 'catui-weapp/dist/style/components/loading.scss';
 
 const Index: React.FC = () => {
   return (
     <View>
-      <View>
-        <CatLoading>加载中...</CatLoading>
-      </View>
+      <ShowArea title='加载类型' padding>
+        <CatLoading />
+        <CatLoading type='spinner' />
+      </ShowArea>
 
-      <View>
-        <CatLoading vertical={true}>加载中...</CatLoading>
-      </View>
+      <ShowArea title='自定义颜色' padding>
+        <CatLoading color='#1989fa' />
+        <CatLoading color='#1989fa' type='spinner' />
+      </ShowArea>
 
-      <View>
-        <CatLoading color='red' vertical={true}>
+      <ShowArea title='加载文案' padding>
+        <CatLoading type='spinner'>加载中...</CatLoading>
+      </ShowArea>
+
+      <ShowArea title='加载类型' padding>
+        <CatLoading type='spinner' vertical>
           加载中...
         </CatLoading>
-      </View>
-
-      <View>
-        <CatLoading color='red' vertical={true} type='spinner'>
-          加载中...
-        </CatLoading>
-      </View>
+      </ShowArea>
     </View>
   );
 };
