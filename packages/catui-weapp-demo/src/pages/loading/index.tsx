1521b8737effe0a87bb0a116a993ab27065ccd3c
wangjq4214 <w1637894214@163.com>
2020-12-25 17:07:07 +0800

    feat: loading 添加自定义样式

--- packages/catui-weapp-demo/src/pages/loading/index.tsx
+++ packages/catui-weapp-demo/src/pages/loading/index.tsx
@@ -5,26 +5,34 @@ import { CatLoading } from 'catui-weapp';
 import ShowArea from '../../components/showArea';
 
 import 'catui-weapp/dist/style/components/loading.scss';
+import './index.scss';
 
 const Index: React.FC = () => {
   return (
     <View>
       <ShowArea title='加载类型' padding>
-        <CatLoading />
-        <CatLoading type='spinner' />
+        <CatLoading className='demo-loading' />
+        <CatLoading className='demo-loading' type='spinner' />
       </ShowArea>
 
       <ShowArea title='自定义颜色' padding>
-        <CatLoading color='#1989fa' />
-        <CatLoading color='#1989fa' type='spinner' />
+        <CatLoading className='demo-loading' color='#1989fa' />
+        <CatLoading className='demo-loading' color='#1989fa' type='spinner' />
       </ShowArea>
 
       <ShowArea title='加载文案' padding>
-        <CatLoading type='spinner'>加载中...</CatLoading>
+        <CatLoading className='demo-loading' type='spinner' size='24px'>
+          加载中...
+        </CatLoading>
       </ShowArea>
 
       <ShowArea title='加载类型' padding>
-        <CatLoading type='spinner' vertical>
+        <CatLoading
+          className='demo-loading'
+          type='spinner'
+          size='24px'
+          vertical
+        >
           加载中...
         </CatLoading>
       </ShowArea>
